9ee4aab0fff134db368512d261c5557f9634b504
Madhumita Mondal <48853024+madhumita-mondal@users.noreply.github.com>
2023-01-05 19:02:40 -0800

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,107 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Jacket_1",
+    price: 145,
+    desc: "Women's Lightweight Long-Sleeve Full-Zip Water-Resistant Packable Hooded Puffer Jacket",
+    img: "images/img-1.jpeg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Jacket_2",
+    price: 100,
+    desc: "Women's Lightweight Long-Sleeve Full-Zip Water-Resistant Packable Hooded Puffer Jacket",
+    img: "images/img-2.jpeg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Jacket_3",
+    price: 125,
+    desc: "Women's Lightweight Long-Sleeve Full-Zip Water-Resistant Packable Hooded Puffer Jacket",
+    img: "images/img-3.jpeg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Jacket_4",
+    price: 300,
+    desc: "Women's Lightweight Long-Sleeve Full-Zip Water-Resistant Packable Hooded Puffer Jacket",
+    img: "images/img-4.jpeg",
+  },
+  {
+    id: "hgjgkjhkjh",
+    name: "Jacket_5",
+    price: 200,
+    desc: "Women's Lightweight Long-Sleeve Full-Zip Water-Resistant Packable Hooded Puffer Jacket",
+    img: "images/img-5.jpeg",
+  },
+  {
+    id: "thyfhklkigwawq",
+    name: "Jacket_6",
+    price: 350,
+    desc: "Women's Lightweight Long-Sleeve Full-Zip Water-Resistant Packable Hooded Puffer Jacket",
+    img: "images/img-6.jpeg",
+  },
+  {
+    id: "thyfhsdtrd",
+    name: "Jacket_7",
+    price: 300,
+    desc: "Women's Lightweight Long-Sleeve Full-Zip Water-Resistant Packable Hooded Puffer Jacket",
+    img: "images/img-7.jpeg",
+  },
+  {
+    id: "waewstrdt",
+    name: "Jacket_8",
+    price: 150,
+    desc: "Women's Lightweight Long-Sleeve Full-Zip Water-Resistant Packable Hooded Puffer Jacket",
+    img: "images/img-8.jpeg",
+  },
+
+  {
+    id: "waewfhjfgjgku",
+    name: "Jacket_9",
+    price: 150,
+    desc: "Women's Lightweight Long-Sleeve Full-Zip Water-Resistant Packable Hooded Puffer Jacket",
+    img: "images/img-9.jpeg",
+  },
+
+  {
+    id: "wrdt",
+    name: "Jacket_10",
+    price: 150,
+    desc: "Women's Lightweight Long-Sleeve Full-Zip Water-Resistant Packable Hooded Puffer Jacket",
+    img: "images/img-10.jpeg",
+  },
+
+  {
+    id: "waegfjhfjdt",
+    name: "Jacket_11",
+    price: 100,
+    desc: "Women's Lightweight Long-Sleeve Full-Zip Water-Resistant Packable Hooded Puffer Jacket",
+    img: "images/img-11.jpeg",
+  },
+
+  {
+    id: "wargdaadt",
+    name: "Jacket_12",
+    price: 170,
+    desc: "Women's Lightweight Long-Sleeve Full-Zip Water-Resistant Packable Hooded Puffer Jacket",
+    img: "images/img-12.jpeg",
+  },
+
+  {
+    id: "ttttttt",
+    name: "Jacket_13",
+    price: 100,
+    desc: "Women's Lightweight Long-Sleeve Full-Zip Water-Resistant Packable Hooded Puffer Jacket",
+    img: "images/img-13.jpeg",
+  },
+
+  {
+    id: "wppppppp",
+    name: "Jacket_14",
+    price: 250,
+    desc: "Women's Lightweight Long-Sleeve Full-Zip Water-Resistant Packable Hooded Puffer Jacket",
+    img: "images/img-14.jpeg",
+  },
+  
+];
